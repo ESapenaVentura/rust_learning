54d6cb321239736b2b80f81e525206efc2ded237
ESapenaVentura <sapenaventura@gmail.com>
2023-12-13 08:13:12 +0000

    1.3 Hello cargo

---
+++ projects/hello_cargo/target/debug/deps/hello_cargo-0b84cd14c000524a.d
@@ -0,0 +1,5 @@
+/Users/enrique/HumanCellAtlas/rust_learning/projects/hello_cargo/target/debug/deps/libhello_cargo-0b84cd14c000524a.rmeta: src/main.rs
+
+/Users/enrique/HumanCellAtlas/rust_learning/projects/hello_cargo/target/debug/deps/hello_cargo-0b84cd14c000524a.d: src/main.rs
+
+src/main.rs:
---
+++ projects/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/enrique/HumanCellAtlas/rust_learning/projects/hello_cargo/target/debug/hello_cargo: /Users/enrique/HumanCellAtlas/rust_learning/projects/hello_cargo/src/main.rs
